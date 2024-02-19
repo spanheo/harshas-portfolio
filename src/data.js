c2b0972158afd595bc6ce3b29a533e8a64a33476
Harsha Konda <danharshk@gmail.com>
2024-02-19 18:38:46 +1100

    added URL for project 4 todo app

--- src/data.js
+++ src/data.js
@@ -29,7 +29,7 @@ export const projects = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://harshas-to-do.netlify.app/",
   },
 ];
 
